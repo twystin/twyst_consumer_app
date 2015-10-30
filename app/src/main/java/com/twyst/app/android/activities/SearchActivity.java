bbaaf096f73bbf5ae69fc0c85f5b3b2a15fc00b0
Vipul <vipul.sharma2008@gmail.com>
2015-10-30 17:06:30 +0530

    SeachActivity cross show to go back

--- app/src/main/java/com/twyst/app/android/activities/SearchActivity.java
+++ app/src/main/java/com/twyst/app/android/activities/SearchActivity.java
@@ -2,8 +2,10 @@ package com.twyst.app.android.activities;
 
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.appsflyer.AppsFlyerLib;
+import com.twyst.app.android.R;
 
 /**
  * Created by rahuls on 7/9/15.
@@ -19,6 +21,14 @@ public class SearchActivity extends DiscoverActivity{
     protected void onCreate(Bundle savedInstanceState) {
         search = getIntent().getBooleanExtra("Search", false);
         super.onCreate(savedInstanceState);
+
+        findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
     @Override
